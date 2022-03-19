4014e77ba94e4d0c3790690ccf0a6dbb8fb12519
311Volt <113amper@gmail.com>
2022-03-19 12:40:38 +0100

    fix example1

--- examples/src/example1.cpp
+++ examples/src/example1.cpp
@@ -19,24 +19,29 @@ int main()
 	eqIn.registerSource(al::keyb::GetEventSource());
 	eqClk.registerSource(clk.getEventSource());
 	
+
+	clk.start();
 	al::Point txtPos {320, 240};
 
 	while(1) {
-		while(!eqClk.empty())
-			eqClk.wait();
-		ALLEGRO_EVENT ev = eqIn.wait();
-		if(ev.type == ALLEGRO_EVENT_KEY_DOWN) {
-			if(ev.keyboard.keycode == ALLEGRO_KEY_ESCAPE) {
-				return 0;
+		eqClk.wait();
+		eqClk.clear();
+		while(!eqIn.empty()) {
+			auto ev = eqIn.pop();	
+			if(ev.type == ALLEGRO_EVENT_KEY_DOWN) {
+				if(ev.keyboard.keycode == ALLEGRO_KEY_ESCAPE) {
+					return 0;
+				}
+			} else if(ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
+				txtPos = {ev.mouse.x, ev.mouse.y};
 			}
-		} else if(ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
-			txtPos = {ev.mouse.x, ev.mouse.y};
+			
 		}
-		
 		al::Display::Clear(al::Color::RGB(0,0,0));
 		bg.draw({0, 0});
 		fn.draw(fmt::format("This is a test. tick={}", clk.getCount()), al::Color::RGB(255,255,255), txtPos);
 		al::Display::Flip();
+
 	}
 
 	return 0;
