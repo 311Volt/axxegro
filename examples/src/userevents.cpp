0324e07e173f9294dfc48743f26fcb4a70af90bb
311Volt <113amper@gmail.com>
2023-06-17 11:36:42 +0200

    refactor user events

--- examples/src/userevents.cpp
+++ examples/src/userevents.cpp
@@ -10,29 +10,41 @@
  */
 
 //How to create custom user events in axxegro:
-//First, we need an event type ID.
-constexpr ALLEGRO_EVENT_TYPE AXX_EXAMPLE_EVENT = 1437;
 
-//Then we define some data structure for the event.
-struct ExampleEventData {
+//Define a data structure for the event.
+struct ExampleEvent {
+	//Let axxegro know its type ID
+	static constexpr ALLEGRO_EVENT_TYPE EventTypeID = 1437;
+
 	float a,b;
 	std::string msg;
 };
 
-//Finally, we associate one with the other using this template
-using ExampleEventType = al::UserEventType<ExampleEventData, AXX_EXAMPLE_EVENT>;
+/*
+ * Another event. This one is trivially copyable and small enough to fit in
+ * the size of 4 pointers, so axxegro will be smart enough to store it in the
+ * ev.user.data1-data4 fields of the ALLEGRO_USER_EVENT struct, no additional
+ * allocation needed.
+ */
+struct SimpleEvent {
+	static constexpr ALLEGRO_EVENT_TYPE EventTypeID = 1438;
+	int a, b;
+};
 
-/* We will emit user events using a user event source. The UserEventSource
+
+/*
+ * We will emit user events using a user event source. The UserEventSource
  * class can be used standalone, but naturally you can extend it to keep some
- * state: */
+ * state:
+ */
 class MyEventSource: public al::UserEventSource {
 public:
 	float state = 0.5;
 
-	void fire()
+	void customEmitEvent()
 	{
-		ExampleEventData dat {state, 3.1, "this is some text"};
-		this->emitEvent<ExampleEventType>(dat);
+		ExampleEvent dat {state, 3.1, "this is some text"};
+		this->emitEvent(dat);
 		state += 0.5;
 	}
 };
@@ -53,16 +65,16 @@ int main()
 	MyEventSource mySource;
 	evLoop.eventQueue.registerSource(mySource);
 
-	//this will be the handler for the event that we just defined
-	evLoop.eventDispatcher.setEventTypeHandler(AXX_EXAMPLE_EVENT, [&](const ALLEGRO_EVENT& ev){
+	//a handler for ExampleEvent
+	evLoop.eventDispatcher.setEventTypeHandler(ExampleEvent::EventTypeID, [&](const ALLEGRO_EVENT& ev){
 
-		const auto& dat = al::GetUserEventData<ExampleEventType>(ev);
+		const ExampleEvent& evData = al::GetUserEventData<ExampleEvent>(ev);
 
 		//reading the data we put in
 		messages.push_back(al::Format(
 			"[%.2f] Received example event: %.1f %.1f \"%s\"",
 			ev.any.timestamp, 
-			dat.a, dat.b, dat.msg.c_str()
+			evData.a, evData.b, evData.msg.c_str()
 		));
 
 		//accessing the event source itself
@@ -70,17 +82,19 @@ int main()
 			"Custom event source state: %f",
 			dynamic_cast<MyEventSource&>(al::GetUserEventSource(ev)).state
 		));
-
-		//delete old lines
-		while(messages.size() >= 25) {
-			messages.pop_front();
-		}
 	});
 
+	evLoop.eventDispatcher.setEventTypeHandler(SimpleEvent::EventTypeID, [&](const ALLEGRO_EVENT& ev){
+		const SimpleEvent& evData = al::GetUserEventData<SimpleEvent>(ev);
+		messages.push_back(al::Format("simple event received: %d, %d", evData.a, evData.b));
+	});
+
+
 	evLoop.eventDispatcher.setEventTypeHandler(ALLEGRO_EVENT_KEY_CHAR, [&](const ALLEGRO_EVENT& ev){
-		if(ev.keyboard.keycode == ALLEGRO_KEY_ENTER) {
-			//ENTER has been pressed, let's emit the event
-			mySource.fire();
+		if(ev.keyboard.keycode == ALLEGRO_KEY_A) {
+			mySource.customEmitEvent();
+		} else if(ev.keyboard.keycode == ALLEGRO_KEY_Z) {
+			mySource.emitEvent(SimpleEvent{.a = 1, .b = 2});
 		}
 	});
 
@@ -88,13 +102,19 @@ int main()
 	evLoop.loopBody = [&](){
 		al::TargetBitmap.clearToColor(al::RGB(0,0,60));
 
-		font.draw("Press ENTER to emit an event", al::White, {al::CurrentDisplay.width()/2.f, 10.f}, ALLEGRO_ALIGN_CENTER);
+		font.draw("Press A or Z to emit events", al::White, {al::CurrentDisplay.width()/2.f, 10.f}, ALLEGRO_ALIGN_CENTER);
+
+		//delete old lines from log
+		while(messages.size() >= 25) {
+			messages.pop_front();
+		}
 
 		int y = 50;
 		for(auto& line: messages) {
 			font.draw(line, al::White, {50.f, float(y)});
 			y += font.getLineHeight() + 3;
 		}
+
 
 		al::CurrentDisplay.flip();
 	};
--- include/axxegro/core/event/UserEvent.hpp
+++ include/axxegro/core/event/UserEvent.hpp
@@ -2,88 +2,137 @@
 #define INCLUDE_AXXEGRO_EVENT_USEREVENT
 
 #include "../../common.hpp"
+#include "EventSource.hpp"
 
 namespace al {
-	
-	template<typename T, ALLEGRO_EVENT_TYPE _ID> 
-	class UserEventType {
-	public:
-		using DataType = T;
-		static constexpr ALLEGRO_EVENT_TYPE ID = _ID;
-		static constexpr bool HasOptimization = std::is_trivially_copyable_v<T> && sizeof(T) <= 4*sizeof(intptr_t);
-		static constexpr bool CanSkipDtor = std::is_trivially_destructible_v<T>;
 
-		static_assert(ID >= 1024, "Event type IDs less than 1024 are reserved for Allegro5 and addons");
+	template<ALLEGRO_EVENT_TYPE ID>
+	constexpr bool IsIDNonReserved = ID >= 1024;
 
-		static ALLEGRO_EVENT Init()
-		{
-			ALLEGRO_EVENT ev;
-			memset(&ev, 0, sizeof(ev));
-			ev.any.type = ID;
-			return ev;
-		}
+	template<typename T>
+	constexpr bool IsIDPresent = std::is_same_v<std::remove_cvref_t<decltype(T::EventTypeID)>, ALLEGRO_EVENT_TYPE>;
 
-		static ALLEGRO_EVENT Create(std::unique_ptr<T> dat)
-		{
-			//TODO wtf is this static_assert
-			static_assert(std::is_same_v<typename decltype(dat)::deleter_type, std::default_delete<T>>, "custom deleters are not supported");
-			ALLEGRO_EVENT ret = Init();
-			
-			if constexpr(HasOptimization) {
-				memcpy(&ret.user.data1, dat.get(), sizeof(T));
-				dat.reset();
-			} else {
-				ret.user.data1 = (intptr_t)(dat.release());
-			}
-			return ret;
-		}
-
-		static ALLEGRO_EVENT Create(const T& dat)
-		{
-			//TODO wtf is this static_assert
-			static_assert(std::is_copy_constructible_v<T>, "cannot create an event by const-reference from a non-copy constructible type, try the unique_ptr overload");
-			ALLEGRO_EVENT ret = Init();
-			if constexpr(HasOptimization) {
-				memcpy(&ret.user.data1, &dat, sizeof(T));
-			} else {
-				T* obj = new T(dat);
-				ret.user.data1 = (intptr_t)(obj);
-			}
-			return ret;
-		}
+	template<typename T>
+	concept UserEventType = requires {
+		requires std::copy_constructible<T> || std::move_constructible<T>;
+		requires IsIDPresent<T>;
+		requires IsIDNonReserved<T::EventTypeID>;
 	};
 
-	template<typename UEvT>
-	void OptUserEventDtor(ALLEGRO_USER_EVENT* ev)
+	template<typename T>
+	concept UserEventTypeRef = UserEventType<std::remove_cvref_t<T>>;
+
+	template<UserEventType T>
+	constexpr bool CanStoreInDataFields = std::is_trivially_copyable_v<T> && sizeof(T) <= 4*sizeof(intptr_t);
+
+
+	template<UserEventType EventT>
+	inline ALLEGRO_EVENT InitUserEvent()
 	{
-		using DT = typename UEvT::DataType;
-		DT* obj = (DT*)(&ev->data1);
-		obj->~DT();
-	}
-	
-	template<typename UEvT>
-	void DefaultUserEventDtor(ALLEGRO_USER_EVENT* ev)
-	{
-		using DT = typename UEvT::DataType;
-		DT* obj = (DT*)(ev->data1);
-		delete obj;
+		ALLEGRO_EVENT ev;
+		memset(&ev, 0, sizeof(ev));
+		ev.any.type = EventT::EventTypeID;
+		return ev;
 	}
 
-	template<typename UEvT>
-	const typename UEvT::DataType& GetUserEventData(const ALLEGRO_EVENT& ev)
+	template<UserEventTypeRef EventRefT>
+	inline ALLEGRO_EVENT CreateUserEvent(EventRefT&& ref)
 	{
-#ifndef NDEBUG
-		if(ev.type != UEvT::ID) {
-			throw EventQueueError("User event ID mismatch");
-		}
-#endif
-		using DT = typename UEvT::DataType;
-		if constexpr(UEvT::HasOptimization) {
-			return *(DT*)(&ev.user.data1);
+		using EventT = std::remove_cvref_t<EventRefT>;
+		ALLEGRO_EVENT ret = InitUserEvent<EventT>();
+
+		if constexpr(CanStoreInDataFields<EventT>) {
+			memcpy(&ret.user.data1, &ref, sizeof(EventT));
 		} else {
-			return *(DT*)(ev.user.data1);
+			ret.user.data1 = (intptr_t)(new EventT(std::forward<EventRefT>(ref)));
+		}
+		return ret;
+	}
+
+
+	template<UserEventType EventT>
+	void UserEventDtor(ALLEGRO_USER_EVENT* ev)
+	{
+		if constexpr (CanStoreInDataFields<EventT>) {
+			auto* obj = (EventT*)(ev->data1);
+			delete obj;
+		} else {
+			// trivially copyable implies trivially destructible - nothing to do
 		}
 	}
+
+	template<UserEventType EventT>
+	EventT* GetUserEventPtr(const ALLEGRO_USER_EVENT* ev)
+	{
+		if constexpr (CanStoreInDataFields<EventT>) {
+			return (EventT*)(&ev->data1);
+		} else {
+			return (EventT*)(ev->data1);
+		}
+	}
+
+	template<UserEventType EventT>
+	const EventT& GetUserEventData(const ALLEGRO_EVENT& ev)
+	{
+		if(ev.type != EventT::EventTypeID) {
+			throw EventQueueError("User event ID mismatch");
+		}
+		return *GetUserEventPtr<EventT>(&ev.user);
+	}
+
+	template<UserEventType EventT>
+	std::optional<std::reference_wrapper<const EventT>> TryGetUserEventData(const ALLEGRO_EVENT& ev) {
+		if(ev.type != EventT::EventTypeID) {
+			return std::nullopt;
+		}
+		return *GetUserEventPtr<EventT>(&ev.user);
+	}
+
+	class UserEventSource:
+			RequiresInitializables<CoreAllegro>,
+			public EventSource
+	{
+		ALLEGRO_EVENT_SOURCE evs {};
+	public:
+		UserEventSource(const UserEventSource&) = delete;
+		UserEventSource& operator=(const UserEventSource&) = delete;
+
+		UserEventSource() {
+			al_init_user_event_source(&evs);
+			al_set_event_source_data(&evs, (intptr_t)this);
+		}
+
+		~UserEventSource() {
+			al_destroy_user_event_source(&evs);
+		}
+
+		[[nodiscard]] ALLEGRO_EVENT_SOURCE* ptr() const override {
+			return (ALLEGRO_EVENT_SOURCE*)(&evs);
+		}
+
+		template<UserEventTypeRef EventRefT>
+		void emitEvent(EventRefT&& event) {
+			using EventT = std::remove_cvref_t<EventRefT>;
+			ALLEGRO_EVENT ev = CreateUserEvent(std::forward<EventRefT>(event));
+			emitAllegroEvent<EventT>(ev);
+		}
+
+	private:
+		template<UserEventType EventT>
+		void emitAllegroEvent(ALLEGRO_EVENT& ev)
+		{
+			al_emit_user_event(&evs, &ev, UserEventDtor<EventT>);
+		}
+
+	};
+
+	inline UserEventSource& GetUserEventSource(const ALLEGRO_EVENT& ev) {
+		if(ev.type < 1024) {
+			throw EventSourceError("GetUserEventSource called on non-user event");
+		}
+		return *(UserEventSource*)al_get_event_source_data(ev.any.source);
+	}
+
 }
 
 #endif /* INCLUDE_AXXEGRO_EVENT_USEREVENT */
