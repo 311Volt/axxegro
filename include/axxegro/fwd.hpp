84fda7b73437a855b8ee979ae5ff3f5aad9acf0c
311Volt <113amper@gmail.com>
2023-08-24 16:18:40 +0200

    add fwd.hpp

---
+++ include/axxegro/fwd.hpp
@@ -0,0 +1,96 @@
+//
+// Created by volt on 2023-08-24.
+//
+
+#ifndef AXXEGRO_FWD_HPP
+#define AXXEGRO_FWD_HPP
+
+namespace al {
+	class AdapterInfo;
+	struct AudioAddon;
+	struct AudioCodecAddon;
+	struct AudioFormat;
+	struct AudioFragmentFormat;
+	struct AudioStreamEventSource;
+	class BaseAudioStream;
+	class BaseLockedBitmapRegion;
+	class Bitmap;
+	struct Blender;
+	struct BufferConfig;
+	class CDefaultVoice;
+	class Color;
+	class Config;
+	struct ConfigEntry;
+	struct ConfigEntryIterator;
+	struct ConfigKey;
+	struct ConfigSectionEntriesView;
+	struct ConfigSectionIterator;
+	struct ConfigSectionView;
+	struct ConfigValue;
+	struct CoreAllegro;
+
+	class Display;
+	class DisplayBackbuffer;
+	class DisplayEventSource;
+	class EventDispatcher;
+	class EventLoop;
+	struct EventLoopConfig;
+	struct EventOwner;
+	class EventQueue;
+	class EventSource;
+	struct Exception;
+	class FileDialog;
+	struct FileDialogResult;
+
+	class Font;
+	struct FontAddon;
+	struct FPSCounter;
+	struct FramerateLimiter;
+	class GenericEventHandler;
+	struct IEventHandler;
+	struct ImageAddon;
+	struct KeyboardDriver;
+	class KeyboardEventSource;
+	class Mixer;
+	class MouseCursor;
+	struct MouseDriver;
+	class MouseEventSource;
+	struct MouseState;
+	struct NativeDialogAddon;
+	struct PixelABGR_F32;
+	struct PixelARGB8888;
+	struct PixelBGR888;
+	class PixelFormat;
+	struct PixelRGB888;
+	struct PixelRGBA8888;
+	struct PlaybackParams;
+	struct PrimitivesAddon;
+	class Sample;
+	struct SampleID;
+	class SampleInstance;
+	struct ScopedBlender;
+	class ScopedNewBitmapFlags;
+	struct ScopedSeparateBlender;
+	class ScopedTargetBitmap;
+	class ScopedTransform;
+	struct SeparateBlender;
+	class Shader;
+	struct StrHash;
+	class SubBitmap;
+	class TextLog;
+	class TextLogEventSource;
+	class Timer;
+	class TimerEventSource;
+	class Transform;
+	struct TTFAddon;
+	class UserEventSource;
+	class UStr;
+	struct Vertex;
+	class VertexDecl;
+	class Video;
+	struct VideoAddon;
+	class VideoEventSource;
+	class Voice;
+}
+
+#endif //AXXEGRO_FWD_HPP
